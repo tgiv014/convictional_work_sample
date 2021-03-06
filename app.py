2297035482003f6484b78215da8fa4817a23888f
Thomas Gorham <tgiv014@gmail.com>
2021-03-06 08:50:02 -0600

    Quick readability changes

--- app.py
+++ app.py
@@ -7,13 +7,17 @@ product_collection = ProductCollection()
 
 app = Flask(__name__)
 
+
 @app.route('/product')
 def get_all_products():
     products = product_collection.get_all()
-    filled_product_schemas = [schemas.map_product_schema(product) for product in products]
+    filled_product_schemas = [schemas.map_product_schema(
+        product) for product in products]
     return jsonify(filled_product_schemas), 200
 
 # Intentionally not using `<int:productId>` here to control the HTTP status code returned
+
+
 @app.route('/product/<productId>')
 def get_product(productId):
     if re.match(r'[0-9]+', productId) is None:
@@ -29,11 +33,13 @@ def get_product(productId):
 
     return jsonify(filled_product_schema), 200
 
+
 @app.route('/store/inventory')
 def get_inventory():
     products = product_collection.get_all()
     inventory = schemas.map_inventory_schema(products)
     return jsonify(inventory), 200
+
 
 if __name__ == '__main__':
     app.run()
--- schemas.py
+++ schemas.py
@@ -15,11 +15,13 @@ def build_images_schema(variants):
             )
     return images
 
+
 def build_weight_schema(variant):
     return {
         'value': variant['weight'],
         'unit': variant['weight_unit']
     }
+
 
 def map_variant_schema(variant):
     return {
@@ -31,9 +33,11 @@ def map_variant_schema(variant):
         'weight': build_weight_schema(variant)
     }
 
+
 def map_product_schema(product):
     return {
-        'code': str(product['id']), # This mapping is an assumption. There is not a code/UPC field in the sample JSON
+        # This mapping is an assumption. There is not a code/UPC field in the sample JSON
+        'code': str(product['id']),
         'title': product['title'],
         'vendor': product['vendor'],
         'bodyHtml': product['body_html'],
@@ -41,15 +45,16 @@ def map_product_schema(product):
         'images': build_images_schema(product['variants'])
     }
 
+
 def map_inventory_schema(products):
     inventory = []
     for product in products:
-            for variant in product['variants']:
-                inventory.append(
-                    {
-                        'productId': str(product['id']),
-                        'variantId': str(variant['id']),
-                        'stock': 0 # There does not appear to be a stock-related field in the sample JSON
-                    }
-                )
+        for variant in product['variants']:
+            inventory.append(
+                {
+                    'productId': str(product['id']),
+                    'variantId': str(variant['id']),
+                    'stock': 0  # There does not appear to be a stock-related field in the sample JSON
+                }
+            )
     return inventory
